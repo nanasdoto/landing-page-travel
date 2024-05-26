3911ed02fa637983c415c7b79986d68276fca4d9
Nanas <akmalun.nas1@gmail.com>
2024-05-26 18:17:41 +0700

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
         xorange: ["#FF5722"],
         xblack: ["#191825"],
       },
+
+      dropShadow :{
+        xsmall: ["0px 856px 240px 0px rgba(0, 0, 0, 0.00), 0px 548px 219px 0px rgba(0, 0, 0, 0.01), 0px 308px 185px 0px rgba(0, 0, 0, 0.04), 0px 137px 137px 0px rgba(0, 0, 0, 0.06), 0px 34px 75px 0px rgba(0, 0, 0, 0.07), 0px 0px 0px 0px rgba(0, 0, 0, 0.07)"],
+      }
     },
   },
   plugins: [],
